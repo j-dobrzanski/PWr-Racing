6e91eff500fc5cb3a062750fdf275fe147eb95f1
j-dobrzanski <j.s.dobrzanski@gmail.com>
2023-03-23 16:00:11 +0100

    Added most of functions implementations

--- geometry.c
+++ geometry.c
@@ -2,12 +2,6 @@
 #include <stdlib.h>
 #include <math.h>
 
-// LOCAL FUNCTIONS DECLARATIONS
-
-// LOCAL FUNCTIONS DEFINITIONS
-
-// GLOBAL FUNCTIONS DEFINITIONS
-
 struct geometry_point {
     double x;
     double y;
@@ -24,6 +18,12 @@ struct geometry_triangle {
     geometry_point* third;
     bool is_right;
 };
+
+// LOCAL FUNCTIONS DECLARATIONS
+
+// LOCAL FUNCTIONS DEFINITIONS
+
+// GLOBAL FUNCTIONS DEFINITIONS
 
 /**
 *   Function to create new gemetry_point object with given coordinates
@@ -115,6 +115,32 @@ void geometry_point_moveByVector(geometry_point* point, double vector_x, double 
     }
     point->x += vector_x;
     point->y += vector_y;
+}
+
+/**
+*   Function to rotate point through an angle around another point
+*   User is expected to provide angle measured in radians
+*   calculated counterclockwise
+*   In params:
+*       geometry_point* rotated_point       point to be rotated
+*       double angle                        angle to rotate through in radians calculated counterclockwise
+*       geometry_point* reference_point     point around which rotations will be calculated
+*
+*   Out params/return:
+*       none (roated_point object is changed)       
+*/
+void geometry_point_rotateByAngle(geometry_point* rotated_point, double angle, geometry_point* reference_point){
+    if(rotated_point == NULL || reference_point == NULL){
+        return;
+    }
+    double rotate_x = rotated_point->x;
+    double rotate_y = rotated_point->y;
+    double reference_x = reference_point->x;
+    double reference_y = reference_point->y;
+    rotated_point->x = (rotate_x - reference_x) * cos(angle) - (rotate_y - reference_y) * sin(angle) + reference_x;
+    rotated_point->y = (rotate_x - reference_x) * sin(angle) + (rotate_y - reference_y) * cos(angle) + reference_y;
+    // For comments on these equations please refer to documentation
+    // TODO: add testcases!!
 }
 
 /**
@@ -229,6 +255,29 @@ void geometry_segment_moveByVector(geometry_segment* segment, double vector_x, d
 }
 
 /**
+*   Function to rotate segment through an angle around another point
+*   User is expected to provide angle measured in radians
+*   calculated counterclockwise
+*   In params:
+*       geometry_segment* rotated_segment       segment to be rotated
+*       double angle                            angle to rotate through in radians calculated counterclockwise
+*       geometry_point* reference_point         point around which rotations will be calculated
+*
+*   Out params/return:
+*       none (roated_segment object is changed)       
+*/
+void geometry_segment_rotateByAngle(geometry_segment* rotated_segment, double angle, geometry_point* reference_point){
+    if(rotated_segment == NULL || reference_point == NULL){
+        return;
+    }
+    geometry_point* start = rotated_segment->start;
+    geometry_point* end = rotated_segment->end;
+    geometry_point_rotateByAngle(start, angle, reference_point);
+    geometry_point_rotateByAngle(end, angle, reference_point);
+    // TODO: add testcases!!
+}   
+
+/**
 *   Function to calculate length of given segment
 *   In params:
 *       geometry_segment* segment       segment to calculate its length
@@ -247,6 +296,68 @@ double geometry_segment_calculateLength(geometry_segment* segment){
     geometry_point* end = NULL;
     geometry_segment_getPoints(segment, &start, &end);
     return geometry_point_calculateDistance(start, end);
+}
+
+/**
+*   Function to determine if given segments are parrarel
+*   In params:
+*       geometry_segment* first_segment         first segment
+*       geometry_segment* second_segment        second_segment
+*
+*   Out params:
+*       none
+*
+*   Return:
+*       bool                                    true if given segments are parallel, false otherwise
+*/
+bool geometry_segment_areParallel(geometry_segment* first_segment, geometry_segment* second_segment){
+    if(first_segment == NULL || second_segment== NULL){
+        return false;
+    }
+    return (first_segment->end->x - first_segment->start->x)*(first_segment->start->y - first_segment->end->y) ==
+                (first_segment->end->y - first_segment->start->y)*(second_segment->start->x - second_segment->end->x);
+    // For comments on theese equations please refer to documentation
+    // TODO: add testcases!!
+}
+
+/**
+*   Function to determine if given segments are perpendicular
+*   In params:
+*       geometry_segment* first_segment         first segment
+*       geometry_segment* second_segment        second_segment
+*
+*   Out params:
+*       none
+*
+*   Return:
+*       bool                                    true if given segments are perpendicular, false otherwise
+*/
+bool geometry_segment_arePerpendicular(geometry_segment* first_segment, geometry_segment* second_segment){
+    if(first_segment == NULL || second_segment== NULL){
+        return false;
+    }
+    return (first_segment->end->x - first_segment->start->x)*(second_segment->start->x - second_segment->end->x) == 
+                -(first_segment->end->y - first_segment->start->y)*(second_segment->start->y - second_segment->end->y);
+    // For comments on theese equations please refer to documentation
+    // TODO: add testcases!!
+}
+
+/**
+*   Function to calculate intersection point of given segments
+*   In params:
+*       geometry_segment* first_segment         first segment
+*       geometry_segment* second_segment        second_segment
+*
+*   Out params:
+*       none
+*
+*   Return:
+*       geometry_point*                         object of geometry_point structure(class) which is interesection
+*                                               of given segments, if segments are parallel, then NULL
+*/
+geometry_point* geometry_segment_calculateIntersection(geometry_segment* first_segment, geometry_segment* second_segment){
+    // TODO
+    return NULL;
 }
 
 /**
@@ -365,6 +476,31 @@ void geometry_triangle_moveByVector(geometry_triangle* triangle, double vector_x
 }
 
 /**
+*   Function to rotate triangle through an angle around another point
+*   User is expected to provide angle measured in radians
+*   calculated counterclockwise
+*   In params:
+*       geometry_triangle* rotated_triangle         triangle to be rotated
+*       double angle                                angle to rotate through in radians calculated counterclockwise
+*       geometry_point* reference_point             point around which rotations will be calculated
+*
+*   Out params/return:
+*       none (roated_triangle object is changed)       
+*/
+void geometry_triangle_rotateByAngle(geometry_triangle* rotated_triangle, double angle, geometry_point* reference_point){
+    if(rotated_triangle == NULL || reference_point == NULL){
+        return;
+    }
+    geometry_point* first = rotated_triangle->first;
+    geometry_point* second = rotated_triangle->second;
+    geometry_point* third = rotated_triangle->third;
+    geometry_point_rotateByAngle(first, angle, reference_point);
+    geometry_point_rotateByAngle(second, angle, reference_point);
+    geometry_point_rotateByAngle(third, angle, reference_point);
+    // TODO: add testcases!!
+}
+
+/**
 *   Function to calculate perimeter of a given triangle
 *   In params:
 *       geometry_triangle* triangle     triangle to calculate its perimeter
@@ -380,15 +516,103 @@ double geometry_triangle_calculatePerimeter(geometry_triangle* triangle){
     if(triangle == NULL){
         return -1;
     }
-    geometry_point* first = NULL;
-    geometry_point* second = NULL;
-    geometry_point* third = NULL;
-    geometry_triangle_getPoints(triangle, &first, &second, &third);
-    double length_1 = geometry_point_calculateDistance(first, second);
-    double length_2 = geometry_point_calculateDistance(first, third);
-    double length_3 = geometry_point_calculateDistance(second, third);
+    double length_1 = geometry_point_calculateDistance(triangle->first, triangle->second);
+    double length_2 = geometry_point_calculateDistance(triangle->first, triangle->third);
+    double length_3 = geometry_point_calculateDistance(triangle->second, triangle->third);
     if(length_1 < 0 || length_2 < 0 || length_3 < 0){
         return -1;
     }
     return length_1 + length_2 + length_3;
+    // TODO: add testcases!!
+}
+
+/**
+*   Function to calculate area of a given triangle
+*   In params:
+*       geometry_triangle* triangle     triangle to calculate its area
+*
+*   Out params:
+*       none
+*
+*   Return:
+*       double                          calculated area of given triangle,
+*                                       -1 if error(s) occured
+*/
+double geometry_triangle_calculateArea(geometry_triangle* triangle){
+    if(triangle == NULL){
+        return -1;
+    }
+    if(triangle->is_right){
+        geometry_segment* side_one = geometry_segment_new(triangle->first, triangle->second);
+        geometry_segment* side_two = geometry_segment_new(triangle->first, triangle->third);
+        if(geometry_segment_arePerpendicular(side_one, side_two)){
+            return geometry_segment_calculateLength(side_one) * geometry_segment_calculateLength(side_two) * 0.5;
+        }
+        geometry_segment* side_three = geometry_segment_new(triangle->second, triangle->third);
+        if(geometry_segment_arePerpendicular(side_one, side_three)){
+            return geometry_segment_calculateLength(side_one) * geometry_segment_calculateLength(side_three) * 0.5;
+        }
+        return geometry_segment_calculateLength(side_two) * geometry_segment_calculateLength(side_three) * 0.5;
+    }
+    else{
+        double side_one_length = geometry_point_calculateDistance(triangle->first, triangle->second);
+        double side_two_length = geometry_point_calculateDistance(triangle->first, triangle->second);
+        double side_three_length = geometry_point_calculateDistance(triangle->second, triangle->third);
+        double half_perimeter = (side_one_length + side_two_length + side_three_length) / 2;
+        return sqrt(half_perimeter * (half_perimeter - side_one_length) * (half_perimeter - side_two_length) * (half_perimeter - side_three_length));
+    }
+    return -1;
+    // For comments on theese equations please refer to documentation
+    // TODO: add testcases!!
+}
+
+/**
+*   Function to determine if two given triangles intersect
+*   In params:
+*       geometry_triangle* first_triangle       first triangle
+*       geometry_triangle* second_triangle      second triangle
+*
+*   Out params:
+*       none
+*
+*   Return:
+*       bool                                    true if triangles are disjoint,
+*                                               false if they intersect
+*/
+bool geometry_triangle_areDisjoint(geometry_triangle* first_triangle, geometry_triangle* second_triangle){
+    // TODO
+    return false;
+}
+
+/**
+*   Function to calculate hypotenuse of a given triangle.
+*   User should provide a right-angled triangle
+*   In params:
+*       geometry_triangle* triangle     triangle to calculate its hypotenuse
+*
+*   Out params:
+*       none
+*
+*   Return:
+*       double                          calculated hypotenuse of given triangle,
+*                                       -1 if given triangle is not right-angled
+*/
+double geometry_triangle_calculateHypotenuse(geometry_triangle* triangle){
+    if(triangle == NULL){
+        return -1;
+    }
+    if(!triangle->is_right){
+        return -1;
+    }
+    geometry_segment* side_one = geometry_segment_new(triangle->first, triangle->second);
+    geometry_segment* side_two = geometry_segment_new(triangle->first, triangle->third);
+    if(geometry_segment_arePerpendicular(side_one, side_two)){
+        return sqrt(pow(geometry_segment_calculateLength(side_one), 2) + pow(geometry_segment_calculateLength(side_two), 2));
+    }
+    geometry_segment* side_three = geometry_segment_new(triangle->second, triangle->third);
+    if(geometry_segment_arePerpendicular(side_one, side_three)){
+        return sqrt(pow(geometry_segment_calculateLength(side_one), 2) + pow(geometry_segment_calculateLength(side_three), 2));
+    }
+    return sqrt(pow(geometry_segment_calculateLength(side_two), 2) + pow(geometry_segment_calculateLength(side_three), 2));
+    // TODO: add testcases!!
 }
